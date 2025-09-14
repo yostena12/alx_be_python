7f8b07acb060e5e7eb069218d0600ff8ebac971c
Yostena Teferi <yostenateferi12@gmail.com>
2025-09-14 12:42:49 +0300

    Add simple interest calculator script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# simple_interest.py
+
+principal = 1000   # Principal amount in dollars
+rate = 0.05        # Annual interest rate (5%)
+time = 3           # Time in years
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
